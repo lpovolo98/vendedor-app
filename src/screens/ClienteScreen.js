bc28de23af482d9b4f17f24572b537568073986a
lpovolo98 <lucas.povolo98@gmail.com>
2026-05-26 02:07:46 -0300

    Update ClienteScreen.js

--- src/screens/ClienteScreen.js
+++ src/screens/ClienteScreen.js
@@ -20,7 +20,7 @@ export default function ClienteScreen() {
       timer.start();
     }
     return () => timer.stop();
-    // eslint-disable-next-line react-hooks/exhaustive-deps
+    // eslint-disable-next-line
   }, [id]);
 
   if (!cliente) {
